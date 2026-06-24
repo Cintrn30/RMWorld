db142ec342e4bad4bb875f3049cc3ccf438dc8f7
Cintrn30 <christiancn31@gmail.com>
2026-06-24 09:16:42 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,2 @@
-const searchInput = document.getElementById("searchInput");
 
-if(searchInput){
-
-searchInput.addEventListener("keyup", function(){
-
-let filter = this.value.toLowerCase();
-
-let cards = document.querySelectorAll(".card");
-
-cards.forEach(card => {
-
-let text = card.innerText.toLowerCase();
-
-card.style.display =
-text.includes(filter) ? "block" : "none";
-
-});
-
-});
-
-}
+console.log('RMWorld loaded');
